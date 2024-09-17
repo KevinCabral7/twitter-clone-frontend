a1f02ceaa7944acbcbac2a6cf647eadf5a3fa02a
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-17 20:30:10 -0300

    changes on eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    base: '/',
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-    },
-    preview: {
-        host: '0.0.0.0',
-        port: 8000,
-    },
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    const Port = `${env.PORT ?? '3000'}`;
+    return defineConfig({
+        base: '/',
+        plugins: [react()],
+        server: {
+            host: '0.0.0.0',
+            port: 3000,
+        },
+        preview: {
+            host: '0.0.0.0',
+            port: Port,
+        },
+    });
+};
