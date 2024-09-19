33250c9e780ac3fd86b69fc8635cae9445b81c60
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-19 16:08:53 -0300

    remove websocket

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { ACCESS_TOKEN } from './constants';
-export const ws = new WebSocket('ws://127.0.0.1:8000/ws/social_media/');
-ws.onopen = () => {
-    console.log('Connected to WebSocket');
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    ws.send(JSON.stringify({
-        action: 'auth',
-        data: token,
-    }));
-};
-ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    console.log('Received data:', data);
-    console.log(event);
-};
-ws.onclose = () => console.log('WebSocket disconnected');
+// export const ws = new WebSocket('ws://127.0.0.1:8000/ws/social_media/');
+// ws.onopen = () => {
+//     console.log('Connected to WebSocket');
+//     const token = localStorage.getItem(ACCESS_TOKEN);
+//     ws.send(JSON.stringify({
+//         action: 'auth',
+//         data: token,
+//     }));
+// };
+// ws.onmessage = (event) => {
+//     const data = JSON.parse(event.data);
+//     console.log('Received data:', data);
+//     console.log(event);
+// };
+// ws.onclose = () => console.log('WebSocket disconnected');
 import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
